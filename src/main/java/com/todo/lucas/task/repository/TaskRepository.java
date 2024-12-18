21dae4c78f07a08b6cedc6e8bf70c0ca619cf4db
Lucas Eduardo <lesds2001@gmail.com>
2024-12-17 23:35:51 -0300

    Adicinando querys de busca ao repository

--- src/main/java/com/todo/lucas/task/repository/TaskRepository.java
+++ src/main/java/com/todo/lucas/task/repository/TaskRepository.java
@@ -3,10 +3,13 @@ package com.todo.lucas.task.repository;
 import com.todo.lucas.task.domain.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TaskRepository extends JpaRepository<Task, String> {
     Optional<Task> findByTitle(String title);
-    Optional<Task> findByInitialDate(String date);
-    Optional<Task> findByEndDate(String date);
+    Optional<List<Task>> findByInitialDate(String date);
+    Optional<List<Task>> findByEndDate(String date);
+    Optional<List<Task>> findByUserId(String userId);
+
 }
